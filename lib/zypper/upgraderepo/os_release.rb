98be2bde71d95ef3f17c71f68f7225d97cbe47a3
Fabio Mucciante <fabio.mucciante@gmail.com>
2020-12-28 11:06:50 +0100

    Added new OS release version.

--- lib/zypper/upgraderepo/os_release.rb
+++ lib/zypper/upgraderepo/os_release.rb
@@ -8,7 +8,7 @@ module Zypper
 
       attr_reader :custom
 
-      OS_VERSIONS = ['13.1', '13.2', '42.1', '42.2', '42.3', '15.0', '15.1', '15.2']
+      OS_VERSIONS = ['13.1', '13.2', '42.1', '42.2', '42.3', '15.0', '15.1', '15.2', '15.3']
 
 
       def initialize(options)
